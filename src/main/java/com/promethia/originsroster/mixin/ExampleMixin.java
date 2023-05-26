2debeb4d6e05fef17c5184318b54f73579aecbde
PROMETHIA <electriccobras@gmail.com>
2023-05-26 19:44:25 -0400

    Downgraded to 1.19.2, hopefully un-exampled it

--- src/main/java/com/promethia/originsroster/mixin/ExampleMixin.java
+++ src/main/java/com/promethia/originsroster/mixin/ExampleMixin.java
@@ -1,11 +1,13 @@
-package net.fabricmc.example.mixin;
+package com.promethia.originsroster.mixin;
 
-import net.fabricmc.example.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.promethia.originsroster.ExampleMod;
 
 @Mixin(TitleScreen.class)
 public class ExampleMixin {
